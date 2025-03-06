f2c3dd81840ab661bb53806bc83156bc2094e207
NDVCat <ndvcat@gmail.com>
2025-03-06 20:21:44 +0700

    machinelearning

--- app.py
+++ app.py
@@ -14,19 +14,26 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Lấy dữ liệu từ yêu cầu
         data = request.get_json(force=True)
+
+        # Kiểm tra dữ liệu đầu vào có đúng định dạng không
+        if not isinstance(data, dict):
+            return jsonify({'error': 'Invalid input format, expected a JSON object'}), 400
         
         # Chuyển đổi dữ liệu thành DataFrame
-        input_data = pd.DataFrame(data, index=[0])
-        
+        input_data = pd.DataFrame([data])
+
         # Thực hiện dự đoán
         prediction = model.predict(input_data)
-        
-        # Trả về dự đoán dưới dạng phản hồi JSON
-        return jsonify({'Predicted_Oilrate': prediction[0]})
+
+        # Trả về kết quả dự đoán
+        return jsonify({'Predicted_Oilrate': float(prediction[0])})
+
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+        
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5005)
+    port = int(os.environ.get("PORT", 5000))  # Render cung cấp biến PORT
+    app.run(host='0.0.0.0', port=port)
